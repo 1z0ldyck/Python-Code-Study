6b17a88646ca1120455629c6a8bf73a73a7e2d26
1z0ldyck <lucase2040@gmail.com>
2021-12-27 12:02:35 -0300

    study(poo): adding new study files

--- poo/composicao/empresa.py
+++ poo/composicao/empresa.py
@@ -1,29 +1,17 @@
+from funcionario import Funcionario
+
 class Empresa:
   
-  __funcionarios = []
-  
-  def __init__(self, nome_empresa: str, qt_funcionarios: int):
+  def __init__(self, nome_empresa: str):
     """Inicializa uma nova empresa"""
-    self.nome_empresa = nome_empresa
-    self.qt_funcionarios = qt_funcionarios
+    self.nome_empresa: str = nome_empresa
+    self.funcionarios: list = []
     
-  @property
-  def funcionarios(self):
-    """Obtém os funcionários do momento."""
-    return self.__funcionarios
-
-  @funcionarios.setter
-  def funcionarios(self, funcionario):
-    """Adiciona um novo funcionário caso a quantidade de funcionários não estiver no limite."""
-    if(self.qt_funcionarios == len(self.__funcionarios)):
-      return False
-    self.__funcionarios.append(funcionario)
-    
-from funcionario import Funcionario
 
 func1 = Funcionario('Lucas', 20, 'Desenvolvedor')
 func2 = Funcionario('Lucas', 21, 'Desenvolvedor')
-empresa1 = Empresa('Empresa', 1)
+empresa1 = Empresa('Empresa')
 empresa1.funcionarios.append(func1)
 print(empresa1.funcionarios)
-print(empresa1.funcionarios.append(func2))
+empresa1.funcionarios.append(func2)
+print(empresa1.funcionarios)
